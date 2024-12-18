080a2e26e59324d718a10e1f646f4d5902910f50
karinfdez <karinfdez@yahoo.com>
2024-12-18 15:28:37 -0500

    Add express to the lambda function

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
+import express from 'express';
+import http from 'serverless-http'; //to use all express props without running a server on some port
 
-export function handler(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({message: 'hello'}),
-    });
-}
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: 'ok'});
+})
+
+export const handler = http(app);
